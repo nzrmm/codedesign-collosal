220ea7eaa94d9c48869759625d74d8e35e5b9bab
refactoryengineering <refactoryengineering@refactory.id>
2023-08-06 16:05:10 +0700

    style: remove flex in card ui design service

--- src/components/card-ui-design-service/index.tsx
+++ src/components/card-ui-design-service/index.tsx
@@ -12,9 +12,7 @@ type ICardUIDesignService = {
 const CardUIDesignService = ({data}: ICardUIDesignService) => {
   return (
     <div
-      className={cx(
-        'flex-1 bg-white/5 rounded-md border border-white/10 px-5 py-6',
-      )}
+      className={cx('bg-white/5 rounded-md border border-white/10 px-5 py-6')}
     >
       <div className={cx('flex items-center gap-4 mb-4')}>
         <Image width={30} height={30} src={data.icon} alt={data.alt} />
