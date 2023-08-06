d10b39bd8dc6cd10ca5af3740a367ab68ab9f1d9
refactoryengineering <refactoryengineering@refactory.id>
2023-08-06 12:26:22 +0700

    feat: add hero section in services page

---
+++ src/pages/services/index.tsx
@@ -0,0 +1,11 @@
+import {Hero} from '@/screens/services';
+
+const Services = () => {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+};
+
+export default Services;
---
+++ src/screens/services/sections/hero/index.tsx
@@ -0,0 +1,34 @@
+import cx from 'classnames';
+
+import {Typography} from '@/components';
+
+const Hero = () => {
+  return (
+    <div className={cx('px-52 pt-20 mb-24 relative')}>
+      {/* Glassmorphism effect */}
+      <div className={cx('glass-effect glass-effect-secondary01')}></div>
+      <div className={cx('glass-effect glass-effect-primary03')}></div>
+      <div className={cx('glass-effect glass-effect-secondary03')}></div>
+
+      <div
+        className={cx(
+          'flex flex-col items-center pb-24 border-b border-white/10',
+        )}
+      >
+        <Typography
+          variant='textBase'
+          customClassName='!font-semibold !text-primary03 tracking-widest mb-3'
+        >
+          SERVICES
+        </Typography>
+
+        <Typography variant='text4Xl' customClassName='text-center mb-6'>
+          We are here to help solve <br />
+          your company&apos;s problems <br />
+        </Typography>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
